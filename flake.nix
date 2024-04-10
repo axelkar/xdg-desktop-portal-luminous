b669ff09e4f4992b9809df3c313c94bf88e06545
Axel Karjalainen <axel@axka.fi>
2024-04-11 00:07:27 +0300

    add build to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,79 @@
 {
   description = "xdg-desktop-portal-luminous devel";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs, ... }:
     let
-      pkgsFor = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ ];
-        };
+      # System types to support.
+      targetSystems = [ "x86_64-linux" "aarch64-linux" ];
 
-      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs targetSystems;
     in {
-      devShells = nixpkgs.lib.genAttrs targetSystems (system:
-        let pkgs = pkgsFor system;
-        in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.stdenv.mkDerivation rec {
+            pname = "xdg-desktop-portal-luminous";
+            version = "0.1.4";
+
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+              clang
+              cargo
+              rustc
+              rustPlatform.cargoSetupHook
+              rustPlatform.bindgenHook
+
+              meson
+              ninja
+              pkg-config
+              wayland-scanner
+            ];
+
+            cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+              inherit src;
+              hash = "sha256-6zkGl67LwiflnbbEsX25P7gRvW9Tt99wyyGDhn2UOYs=";
+            };
+
+            buildInputs = with pkgs; [
+              pipewire.dev
+              #wayland.dev # is this needed?
+              libxkbcommon
+              #glib.dev # is this needed?
+              pango.dev
+              cairo.dev
+            ];
+
+            meta = with nixpkgs.lib; {
+              description = "An alternative to xdg-desktop-portal-wlr for wlroots compositors";
+              homepage = "https://github.com/";
+            };
+          };
+        }
+      );
+
+      overlays.default = final: prev: {
+        inherit (self.packages.${prev.system}) xdg-desktop-portal-luminous;
+      };
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           default = pkgs.mkShell {
             name = "xdg-desktop-portal-luminous-devel";
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
             nativeBuildInputs = with pkgs; [
               # Compilers
               clang
               cargo
               rustc
+              rustPlatform.bindgenHook
 
               # Libs
               pipewire
@@ -46,6 +96,7 @@
               wayland-scanner
             ];
           };
-        });
+        }
+      );
     };
 }
